9d1c751e1ae3594a2205c56620e8b9281aa7fc89
megalodon009007 <devkatayi024@gmail.com>
2026-01-13 08:05:14 +0100

    Add files via upload

---
+++ gestion_salaries_KATAYI_KABUNANGU_DAVID_L2LMDFASI_TP_MERISE.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost
+-- Généré le : lun. 12 jan. 2026 à 22:33
+-- Version du serveur : 10.4.28-MariaDB
+-- Version de PHP : 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `gestion_salaries_KATAYI_KABUNANGU_DAVID_L2LMDFASI_TP_MERISE`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Competences`
+--
+-- Erreur de lecture de structure pour la table gestion_salaries_KATAYI_KABUNANGU_DAVID_L2LMDFASI_TP_MERISE.Competences : #1142 - La commande &#039;SHOW&#039; est interdite à l&#039;utilisateur: &#039;&#039;@&#039;localhost&#039; sur la table `gestion_salaries_katayi_kabunangu_david_l2lmdfasi_tp_merise`.`Competences`
+-- Erreur de lecture des données pour la table gestion_salaries_KATAYI_KABUNANGU_DAVID_L2LMDFASI_TP_MERISE.Competences : #1064 - Erreur de syntaxe près de &#039;FROM `gestion_salaries_KATAYI_KABUNANGU_DAVID_L2LMDFASI_TP_MERISE`.`Competences`&#039; à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Niveau_Competences`
+--
+-- Erreur de lecture de structure pour la table gestion_salaries_KATAYI_KABUNANGU_DAVID_L2LMDFASI_TP_MERISE.Niveau_Competences : #1142 - La commande &#039;SHOW&#039; est interdite à l&#039;utilisateur: &#039;&#039;@&#039;localhost&#039; sur la table `gestion_salaries_katayi_kabunangu_david_l2lmdfasi_tp_merise`.`Niveau_Competences`
+-- Erreur de lecture des données pour la table gestion_salaries_KATAYI_KABUNANGU_DAVID_L2LMDFASI_TP_MERISE.Niveau_Competences : #1064 - Erreur de syntaxe près de &#039;FROM `gestion_salaries_KATAYI_KABUNANGU_DAVID_L2LMDFASI_TP_MERISE`.`Niveau_Co...&#039; à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Posseder`
+--
+-- Erreur de lecture de structure pour la table gestion_salaries_KATAYI_KABUNANGU_DAVID_L2LMDFASI_TP_MERISE.Posseder : #1142 - La commande &#039;SHOW&#039; est interdite à l&#039;utilisateur: &#039;&#039;@&#039;localhost&#039; sur la table `gestion_salaries_katayi_kabunangu_david_l2lmdfasi_tp_merise`.`Posseder`
+-- Erreur de lecture des données pour la table gestion_salaries_KATAYI_KABUNANGU_DAVID_L2LMDFASI_TP_MERISE.Posseder : #1064 - Erreur de syntaxe près de &#039;FROM `gestion_salaries_KATAYI_KABUNANGU_DAVID_L2LMDFASI_TP_MERISE`.`Posseder`&#039; à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Poste`
+--
+-- Erreur de lecture de structure pour la table gestion_salaries_KATAYI_KABUNANGU_DAVID_L2LMDFASI_TP_MERISE.Poste : #1142 - La commande &#039;SHOW&#039; est interdite à l&#039;utilisateur: &#039;&#039;@&#039;localhost&#039; sur la table `gestion_salaries_katayi_kabunangu_david_l2lmdfasi_tp_merise`.`Poste`
+-- Erreur de lecture des données pour la table gestion_salaries_KATAYI_KABUNANGU_DAVID_L2LMDFASI_TP_MERISE.Poste : #1064 - Erreur de syntaxe près de &#039;FROM `gestion_salaries_KATAYI_KABUNANGU_DAVID_L2LMDFASI_TP_MERISE`.`Poste`&#039; à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Salaire`
+--
+-- Erreur de lecture de structure pour la table gestion_salaries_KATAYI_KABUNANGU_DAVID_L2LMDFASI_TP_MERISE.Salaire : #1142 - La commande &#039;SHOW&#039; est interdite à l&#039;utilisateur: &#039;&#039;@&#039;localhost&#039; sur la table `gestion_salaries_katayi_kabunangu_david_l2lmdfasi_tp_merise`.`Salaire`
+-- Erreur de lecture des données pour la table gestion_salaries_KATAYI_KABUNANGU_DAVID_L2LMDFASI_TP_MERISE.Salaire : #1064 - Erreur de syntaxe près de &#039;FROM `gestion_salaries_KATAYI_KABUNANGU_DAVID_L2LMDFASI_TP_MERISE`.`Salaire`&#039; à la ligne 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
